49284c912cbd79b76dd6f9e88a12e6fabca369b5
greg <greg@greg-2600k>
2017-04-09 21:35:05 +0200

    Validation du titre

--- europarl-tools-rp/src/main/java/be/gdusart/europarltools/rp/scheduling/tasks/RPRuleValidationTask.java
+++ europarl-tools-rp/src/main/java/be/gdusart/europarltools/rp/scheduling/tasks/RPRuleValidationTask.java
@@ -1,6 +1,10 @@
 package be.gdusart.europarltools.rp.scheduling.tasks;
 
+import static org.mockito.Matchers.matches;
+
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -8,6 +12,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.message.BasicHeader;
+import org.apache.http.util.EntityUtils;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +25,8 @@ import be.gdusart.europarltools.services.BatchService;
 public final class RPRuleValidationTask implements Runnable {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RPRuleValidationTask.class);
+	private static final Pattern TITLE_PATTERN = Pattern.compile("<title>(.*?)</title>",
+			Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 
 	private String host;
 	private ReverseProxyRule rule;
@@ -40,11 +47,11 @@ public final class RPRuleValidationTask implements Runnable {
 	@Override
 	public void run() {
 		result.setBaseUrl(host + rule.getBaseUrl());
-		
+
 		if (!StringUtils.isEmpty(rule.getExpectedRedictionUrl())) {
 			result.setDestinationUrl(host + rule.getExpectedRedictionUrl());
 		}
-		
+
 		result.setStatus(TaskStatus.IN_PROGRESS);
 		result = batchService.saveBatchTask(result);
 
@@ -60,7 +67,8 @@ public final class RPRuleValidationTask implements Runnable {
 			result.setHttpStatus(response.getStatusLine().getStatusCode());
 			result.setHttpMessage(response.getStatusLine().getReasonPhrase());
 
-			result.setStatus(result.getHttpStatus() == rule.getExpectedCode() ? TaskStatus.SUCCESS : TaskStatus.FAILED);
+			computeStatus(rule, result, response);
+
 		} catch (Exception e) {
 			LOG.error("Error validating rule {}", rule, e);
 			result.setException(ExceptionUtils.getStackTrace(e));
@@ -72,5 +80,34 @@ public final class RPRuleValidationTask implements Runnable {
 		result.setEndDate(new Date());
 		result = batchService.saveBatchTask(result);
 		LOG.info("Result of URL {}: {}", result.getBaseUrl(), result.getStatus());
+	}
+
+	private void computeStatus(ReverseProxyRule rule, ReverseProxyRuleValidationTask result,
+			CloseableHttpResponse response) {
+		result.setStatus(TaskStatus.SUCCESS);
+		
+		try {
+			if (result.getHttpStatus() != rule.getExpectedCode()) {
+				result.setStatus(TaskStatus.FAILED);
+				result.setException("HTTP Status " + result.getHttpStatus() + " did not match expected one (" + rule.getExpectedCode() +")");
+			}
+
+			if (result.getStatus() == TaskStatus.SUCCESS && !StringUtils.isEmpty(rule.getExpectedTitle())) {
+				String pageContent = EntityUtils.toString(response.getEntity());
+				Matcher matcher = TITLE_PATTERN.matcher(pageContent);
+				if (matcher.find()) {
+					String foundTitle = matcher.group(1);
+					if (!StringUtils.equals(rule.getExpectedTitle(), foundTitle)) {
+						result.setStatus(TaskStatus.FAILED);
+						result.setException("Title did not match. Expected " + StringUtils.abbreviate(rule.getExpectedTitle(), 20) +  
+								"... but was " + StringUtils.abbreviate(foundTitle, 20) + "...");
+					}
+				}
+			}
+		} catch (Exception e) {
+			LOG.error("Error computing task status", e);
+			result.setStatus(TaskStatus.FAILED);
+			result.setException(e.getMessage());
+		}
 	}
 }
