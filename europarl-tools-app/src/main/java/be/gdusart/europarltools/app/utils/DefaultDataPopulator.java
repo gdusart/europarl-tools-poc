ed2014890b86c70c90ae5a6940f3a80f3805220b
greg <greg@greg-2600k>
2017-04-09 21:07:14 +0200

    Data changes

--- europarl-tools-app/src/main/java/be/gdusart/europarltools/app/utils/DefaultDataPopulator.java
+++ europarl-tools-app/src/main/java/be/gdusart/europarltools/app/utils/DefaultDataPopulator.java
@@ -52,16 +52,17 @@ public class DefaultDataPopulator implements CommandLineRunner {
 		rules.add(new ReverseProxyRule("/common_dit/css/common_dit.css"));
 		rules.add(new ReverseProxyRule("/ep_framework/css/notices.css"));
 		rules.add(new ReverseProxyRule("/forms/css/forms_main.css"));
-		rules.add(new ReverseProxyRule("/rwd_site/common/css/atomicdesign.css"));
+//		rules.add(new ReverseProxyRule("/rwd_site/common/css/atomicdesign.css"));
 		tomcat6 = rulesService.saveRuleset(tomcat6);
 
 		ReverseProxyRuleSet planets = new ReverseProxyRuleSet();
 		planets.setRuleSetName("planets");
 		rules = planets.getRules();
-		rules.add(new ReverseProxyRule("/100books/css/100_books.css"));
-		rules.add(new ReverseProxyRule("/100books/img/photo/ckw_or.jpg"));
-		rules.add(new ReverseProxyRule("/100books/js/test.txt"));
-		rules.add(new ReverseProxyRule("/100books/ttf/gill-sans-mt-1361533556.ttf"));
+//		rules.add(new ReverseProxyRule("/100books/css/100_books.css")); // Responsive
+//		rules.add(new ReverseProxyRule("/100books/img/photo/ckw_or.jpg"));
+//		rules.add(new ReverseProxyRule("/100books/js/test.txt"));
+//		rules.add(new ReverseProxyRule("/100books/ttf/gill-sans-mt-1361533556.ttf"));
+		rules.add(new ReverseProxyRule("/100_books/css/100_books_main.css"));
 		rules.add(new ReverseProxyRule("/committees/css/committees_revamped.css"));
 		rules.add(new ReverseProxyRule("/committees/img/background/header_plenary.png"));
 		rules.add(new ReverseProxyRule("/committees/js/hotfix.js"));
@@ -75,13 +76,13 @@ public class DefaultDataPopulator implements CommandLineRunner {
 		rules.add(new ReverseProxyRule("/plenary/js/hotfix.js"));
 		rules.add(new ReverseProxyRule("/plenary/img/banner/banner_next_sitting_Strasbourg.png"));
 		rules.add(new ReverseProxyRule("/rss/css/common_rss.css"));
-		rules.add(new ReverseProxyRule("/rss/js/hotfix.js"));
+//		rules.add(new ReverseProxyRule("/rss/js/hotfix.js"));
 		rules.add(new ReverseProxyRule("/rss/img/ep_syndicationrss_screen2.jpg"));
 		rules.add(new ReverseProxyRule("/rss/css/jquery.nimble.loader.css"));
 		rules.add(new ReverseProxyRule("/thinktank/css/thinktank_main.css"));
 		rules.add(new ReverseProxyRule("/thinktank/js/hotfix.js"));
 		rules.add(new ReverseProxyRule("/thinktank/img/background/gradient_white.png"));
-		rules.add(new ReverseProxyRule("/thinktank/font/MyriadPro-Regular.otf"));
+//		rules.add(new ReverseProxyRule("/thinktank/font/MyriadPro-Regular.otf"));
 		planets = rulesService.saveRuleset(planets);
 
 		planets.getEnvironments().add(prodEnv);
